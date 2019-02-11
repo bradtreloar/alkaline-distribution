5383c8e19aa3fbdc6d03993d983dbe2582dc6f7f
Brad Treloar <brad@treloardigital.com.au>
2019-02-12 02:47:57 +1030

    added conversion script

---
+++ config/convert_sync_to_install.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import yaml
+
+if "install" not in os.listdir("."):
+    os.mkdir("install")
+
+for filename in os.listdir("install"):
+    os.remove(os.path.join("install", filename))
+
+for filename in os.listdir("sync"):
+    ignore = [
+        ".htaccess"
+        "core.extension.yml"
+        "update.settings.yml"
+    ]
+
+    if filename in ignore:
+        continue
+
+    with open(os.path.join("sync", filename), "r") as stream:
+        config = yaml.load(stream)
+
+    if "_core" in config:
+        del config["_core"]
+
+    if "uuid" in config:
+        del config["uuid"]
+
+    with open(os.path.join("install", filename), "w") as stream:
+        yaml.dump(config, stream)
