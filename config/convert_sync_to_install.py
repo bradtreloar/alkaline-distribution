34ab32303f2957de67cc985864f42a2e870ff868
Brad Treloar <brad@treloardigital.com.au>
2019-02-14 20:51:03 +1030

    added update.settings

--- config/convert_sync_to_install.py
+++ config/convert_sync_to_install.py
@@ -1,30 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import os
+import re
 import yaml
+
+sync_dir = "sync"
+install_dir = "install"
+
+ignore_filename_patterns = [
+    "\.htaccess",
+    "core\.extension\.yml",
+    "block\.block\.bartik_\.[.]+\.yml",
+    "block\.block\.seven_\.[.]+\.yml"
+]
 
 if "install" not in os.listdir("."):
     os.mkdir("install")
 
-for filename in os.listdir("install"):
-    os.remove(os.path.join("install", filename))
+for filename in os.listdir(install_dir):
+    os.remove(os.path.join(install_dir, filename))
 
-for filename in os.listdir("sync"):
-    ignore = [
-        ".htaccess",
-        "core.extension.yml",
-        "update.settings.yml"
-    ]
+for filename in os.listdir(sync_dir):
+    ignore = False
+    for pattern in ignore_filename_patterns:
+        if re.match(pattern, filename):
+            ignore = True
 
-    if filename not in ignore:
-        with open(os.path.join("sync", filename), "r") as stream:
-            config = yaml.load(stream)
+    if ignore:
+        continue
 
-        if "_core" in config:
-            del config["_core"]
+    with open(os.path.join(sync_dir, filename), "r") as stream:
+        config = yaml.load(stream)
 
-        if "uuid" in config:
-            del config["uuid"]
+    if "_core" in config:
+        del config["_core"]
 
-        with open(os.path.join("install", filename), "w") as stream:
-            yaml.dump(config, stream)
+    if "uuid" in config:
+        del config["uuid"]
+
+    with open(os.path.join(install_dir, filename), "w") as stream:
+        yaml.dump(config, stream)
