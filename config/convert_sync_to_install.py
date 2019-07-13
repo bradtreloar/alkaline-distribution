c9b4f9ed9913cab0e1a59fd57623aef26eab0ab2
Brad Treloar <brad@treloardigital.com.au>
2019-07-13 22:33:46 +0930

    Moved ignore logic to .gitignore

--- config/convert_sync_to_install.py
+++ config/convert_sync_to_install.py
@@ -7,53 +7,27 @@ from ruamel.yaml import YAML
 sync_dir = "../../../../../config/sync"
 install_dir = "install"
 
-ignore_filename_patterns = [
-    "\.htaccess",
-    "README\.txt",
-
-    # Do not use core.extension config as this configuration is created during
-    # site installation.
-    "core\.extension\.yml",
-
-    # Do not use google_analytics.settings as this config contains secrets
-    "google_analytics\.settings\.yml",
-
-    # Do not use google_map_field.settings as this config contains secrets
-    "google_map_field\.settings\.yml",
-
-    # Ignore block configuration for any theme besides the alkaline base theme
-    # and the core themes, Bartik and Seven.
-    "block\.block\.(?!bartik)(?!seven)[a-z]+[^\.]+.yml",
-
-    # Do not use update.settings config as this configuration is created during
-    # site installation.
-    "update\.settings\.yml",
-]
-
 if "install" not in os.listdir("."):
     os.mkdir("install")
 
+# Delete the existing configuration in this profile.
 for filename in os.listdir(install_dir):
     os.remove(os.path.join(install_dir, filename))
 
+# Copy the exported configuration to this profile
 for filename in os.listdir(sync_dir):
-    ignore = False
-    for pattern in ignore_filename_patterns:
-        if re.match(pattern, filename):
-            ignore = True
-
-    if ignore:
-        continue
-
+    # Load the config file.
     with open(os.path.join(sync_dir, filename), "r") as stream:
         yaml = YAML()
         config = yaml.load(stream)
 
+    # Strip out the _core and uuid entries because these are not allowed
+    # in an profile's config.
     if "_core" in config:
         del config["_core"]
-
     if "uuid" in config:
         del config["uuid"]
 
+    # Save the config file.
     with open(os.path.join(install_dir, filename), "w") as stream:
         yaml.dump(config, stream)
